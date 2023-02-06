aedf944df1cb3ebbcfcf80d7e570b2864abd72b8
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-02-06 13:13:31 -0500

    Update engineer.test.js

--- _test_/engineer.test.js
+++ _test_/engineer.test.js
@@ -3,19 +3,19 @@ const Engineer = require('../lib/engineer');
 
 //Test
 test("Test Engineer's github", () => {
-    const github = "VimukthiGunasekara";
-    const eGitHub = new Engineer("Vimukthi", 1, "Vimo@gmail.com", github);
+    const github = "Sviatoslav Zakharuk";
+    const eGitHub = new Engineer("Sviatoslav", 1, "Svat@gmail.com", github);
     expect(eGitHub.github).toBe(github);
 });
 
 test("Test getGithub methode in Engineer", () => {
-    const github = "VimukthiGunasekara";
-    const eGitHub = new Engineer("Vimukthi", 1, "Vimo@gmail.com", github);
+    const github = "Sviatoslav Zakharuk";
+    const eGitHub = new Engineer("Sviatoslav", 1, "Svat@gmail.com", github);
     expect(eGitHub.getGithub()).toBe(github);
 });
 
 test("Test getRole method in Engineer", () => {
     const role = "Engineer";
-    const eRole = new Engineer("Vimukthi", 1, "Vimo@gmail.com", role);
+    const eRole = new Engineer("Sviatoslav", 1, "Svat@gmail.com", role);
     expect(eRole.getRole()).toBe(role);
 });
