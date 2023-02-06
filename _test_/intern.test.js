bebbbd243c461392191e915dbd5e266875510c08
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-02-06 13:15:57 -0500

    Update intern.test.js

--- _test_/intern.test.js
+++ _test_/intern.test.js
@@ -1,19 +1,19 @@
 const Intern = require('../lib/intern');
 
 test("Test Intern's School", () => {
-    const school = "University of Toronto";
-    const intern = new Intern("Vimukthi", 1, "Vimo@gmail.com", school);
+    const school = "Carleton University";
+    const intern = new Intern("Sviatoslav", 1, "svat@gmail.com", school);
     expect(intern.school).toBe(school);
 });
 
 test("Test getSchool methode in Intern", () => {
-    const school = "University of Toronto";
-    const intern = new Intern("Vimukthi", 1, "Vimo@gmail.com", school);
+    const school = "Carleton University";
+    const intern = new Intern("Sviatoslav", 1, "svat@gmail.com", school);
     expect(intern.getSchool()).toBe(school);
 });
 
 test("Test getRole method in Intern", () => {
     const role = "Intern";
-    const eRole = new Intern("Vimukthi", 1, "Vimo@gmail.com", role);
+    const eRole = new Intern("Sviatoslav", 1, "Svat@gmail.com", role);
     expect(eRole.getRole()).toBe(role);
 });
