1d77aa13735f0619ded8b947a817785ec6539e0f
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-02-06 13:17:54 -0500

    Update manager.test.js

--- _test_/manager.test.js
+++ _test_/manager.test.js
@@ -2,18 +2,18 @@ const Manager = require("../lib/manager");
 
 test("Test Manager's officeNumber", () => {
     const officeNumber = 9053245755;
-    const eOfficeNumber = new Manager("Vimukthi", 1, "Vimo@gmail.com", officeNumber);
+    const eOfficeNumber = new Manager("Sviatoslav", 1, "svat@gmail.com", officeNumber);
     expect(eOfficeNumber.officeNumber).toBe(officeNumber);
 });
 
 test("Test getOfficeNumber method in Manager", () => {
     const officeNumber = 9053245755;
-    const eOfficeNumber = new Manager("Vimukthi", 1, "Vimo@gmail.com", officeNumber);
+    const eOfficeNumber = new Manager("Sviatoslav", 1, "svat@gmail.com", officeNumber);
     expect(eOfficeNumber.getOfficeNumber()).toBe(officeNumber);
 });
 
 test("Test getRole method in Manager", () => {
     const role = "Manager";
-    const eRole = new Manager("Vimukthi", 1, "Vimo@gmail.com", role);
+    const eRole = new Manager("Sviatoslav", 1, "Svat@gmail.com", role);
     expect(eRole.getRole()).toBe(role);
 });
